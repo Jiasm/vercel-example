68e129bfa04380017018a6306269c29a4aa6d665
jiashunming <jiashunming@kuaishou.com>
2024-01-16 11:30:14 +0800

    fix: using absolute file path

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
   if (req.url === '/ping') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('pong');
   } else if (req.url === '/') {
-    fs.readFile('../client/index.html', 'utf8', (err, data) => {
+    fs.readFile(path.resolve(__dirname, '../client/index.html'), 'utf8', (err, data) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
